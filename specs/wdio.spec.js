695bc42887067552d3305337f38dc0bf5daabea3
AnnaShtan <74980452+AnnaShtan@users.noreply.github.com>
2021-07-17 23:01:12 +0300

    Add files via upload
    My homework

--- specs/wdio.spec.js
+++ specs/wdio.spec.js
@@ -4,7 +4,7 @@ const rundomNumber = () => Date.now();
 
 describe('Registration:', function () {
 
-  it('should be able to register', async function () {
+  xit('should be able to register', async function () {
 
     await browser.setWindowSize(1440, 960);
     await browser.url('/sign-up');
@@ -73,7 +73,237 @@ describe('Registration:', function () {
 
     const url = await browser.getUrl();
     expect(url).to.be.eql('http://46.101.234.121/doctors');
-    
+
+    await browser.reloadSession();
+  });
+});
+
+
+// 1. Тест на логин.
+describe('Sign In:', function () {
+  it('should be able to sign in', async function () {
+
+    await browser.setWindowSize(1440, 960);
+    await browser.url('/sign-in');
+
+    const emailField = await $('input[name="email"]');
+    const passwordField = await $('input[name="password"]');
+
+    const signInButton = await $('button');
+
+    await emailField.waitForDisplayed({ timeout: 5000 });
+    await emailField.setValue(`john_admin1@admin.com`);
+
+    await passwordField.waitForDisplayed({ timeout: 5000 });
+    await passwordField.setValue('Pa55word');
+
+    await signInButton.waitForDisplayed({ timeout: 5000 });
+    await signInButton.click();
+
+    await browser.waitUntil(
+      async function () {
+        const url = await browser.getUrl();
+        return url === 'http://46.101.234.121/doctors';
+      },
+      { timeout: 5000 },
+    );
+
+    const url = await browser.getUrl();
+    expect(url).to.be.eql('http://46.101.234.121/doctors');
+
+    await browser.reloadSession();
+  });
+
+});
+
+
+// 2. Тест на логин с невалидными данными.
+describe('Sign In with non valid data:', function () {
+  xit('should not be able to sign in (with invalid password)', async function () {
+
+    await browser.setWindowSize(1440, 960);
+    await browser.url('/sign-in');
+
+    const emailField = await $('input[name="email"]');
+    const passwordField = await $('input[name="password"]');
+
+    const signInButton = await $('button');
+
+    await emailField.waitForDisplayed({ timeout: 5000 });
+    await emailField.setValue(`john_admin1@admin.com`);
+
+    await passwordField.waitForDisplayed({ timeout: 5000 });
+    await passwordField.setValue('Pa55word');
+
+    await signInButton.waitForDisplayed({ timeout: 5000 });
+    await signInButton.click();
+
+
+
+    await browser.waitUntil(
+      async function () {
+        const url = await browser.getUrl();
+        return url === 'http://46.101.234.121/sign-in';
+      },
+      { timeout: 5000 },
+    );
+
+    const url = await browser.getUrl();
+    expect(url).to.be.eql('http://46.101.234.121/sign-in');
+
+    await browser.reloadSession();
+  });
+
+});
+
+
+//3. Тест на изменение данных в профиле пользователя.
+
+describe('Changing data in user profile:', function () {
+  xit('should be able to change the data in user profile', async function () {
+
+    await browser.setWindowSize(1440, 960);
+    await browser.url('/sign-in');
+
+    const emailField = await $('input[name="email"]');
+    const passwordField = await $('input[name="password"]');
+
+    const signInButton = await $('button');
+
+    const myProfile = await $('//a[contains(text(), "My Profile")]');
+
+    const buttonEdit = await $('button.styles_edit__ftuHl');
+
+    const surnameField = await $('input[name="surname"]');
+
+    const ddls = await $$('button');
+    const submitButton = ddls[5];
+
+    await emailField.waitForDisplayed({ timeout: 5000 });
+    await emailField.setValue(`john_admin1@admin.com`);
+
+    await passwordField.waitForDisplayed({ timeout: 5000 });
+    await passwordField.setValue('Pa55word');
+
+    await signInButton.waitForDisplayed({ timeout: 5000 });
+    await signInButton.click();
+
+    await myProfile.waitForDisplayed({ timeout: 5000 });
+    await myProfile.click();
+
+
+    await buttonEdit.waitForDisplayed({ timeout: 5000 });
+    await buttonEdit.click();
+
+
+    await surnameField.waitForDisplayed({ timeout: 5000 });
+    await surnameField.setValue("Ukolov");
+
+
+    await submitButton.waitForDisplayed({ timeout: 5000 });
+    await submitButton.click();
+
+    await browser.waitUntil(
+      async function () {
+        const url = await browser.getUrl();
+        return url === 'http://46.101.234.121/user-profile/aa5058a3-3e09-4db4-b8fb-2232cc612265';
+      },
+      { timeout: 5000 },
+    );
+
+    const url = await browser.getUrl();
+    expect(url).to.be.eql('http://46.101.234.121/user-profile/aa5058a3-3e09-4db4-b8fb-2232cc612265');
+
+    await browser.reloadSession();
+  });
+
+});
+
+//4. Тест на изменение специальности и клиники для доктора.
+
+describe('Changing doctor_s specialty and clinic:', function () {
+  xit('should be able to change doctor_s specialty and clinic in user profile', async function () {
+
+    await browser.setWindowSize(1440, 960);
+    await browser.url('/sign-in');
+
+    const emailField = await $('input[name="email"]');
+    const passwordField = await $('input[name="password"]');
+
+    const signInButton = await $('button');
+
+    const myProfile = await $('//a[contains(text(), "My Profile")]');
+
+    await emailField.waitForDisplayed({ timeout: 5000 });
+    await emailField.setValue(`john_admin1@admin.com`);
+
+    await passwordField.waitForDisplayed({ timeout: 5000 });
+    await passwordField.setValue('Pa55word');
+
+    await signInButton.waitForDisplayed({ timeout: 5000 });
+    await signInButton.click();
+
+    await myProfile.waitForDisplayed({ timeout: 5000 });
+    await myProfile.click();
+
+
+
+    await browser.waitUntil(
+      async function () {
+        const url = await browser.getUrl();
+        return url === 'http://46.101.234.121/user-profile/aa5058a3-3e09-4db4-b8fb-2232cc612265';
+      },
+      { timeout: 5000 },
+    );
+
+    const url = await browser.getUrl();
+    expect(url).to.be.eql('http://46.101.234.121/user-profile/aa5058a3-3e09-4db4-b8fb-2232cc612265');
+
+    await browser.reloadSession()
+  });
+});
+
+
+//5. Тест на добавление новой клиники.
+describe('Add new clinic:', function () {
+  xit('should be able to add new clinic', async function () {
+
+    await browser.setWindowSize(1440, 960);
+    await browser.url('/sign-in');
+
+    const emailField = await $('input[name="email"]');
+    const passwordField = await $('input[name="password"]');
+
+    const signInButton = await $('button');
+
+    const clinicsPage = await $('//a[contains(text(),"Clinics")]');
+
+
+    await emailField.waitForDisplayed({ timeout: 5000 });
+    await emailField.setValue(`john_admin1@admin.com`);
+
+    await passwordField.waitForDisplayed({ timeout: 5000 });
+    await passwordField.setValue('Pa55word');
+
+    await signInButton.waitForDisplayed({ timeout: 5000 });
+    await signInButton.click();
+
+    await clinicsPage.waitForDisplayed({ timeout: 5000 });
+    await clinicsPage.click();
+
+
+
+    await browser.waitUntil(
+      async function () {
+        const url = await browser.getUrl();
+        return url === 'http://46.101.234.121/clinics';
+      },
+      { timeout: 5000 },
+    );
+
+    const url = await browser.getUrl();
+    expect(url).to.be.eql('http://46.101.234.121/clinics');
+
     await browser.reloadSession();
   });
 });
